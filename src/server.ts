b50b9317ddf9c7616ad3ba66ba1f2dd3d493b174
Luis V <luisvinicius0906@gmail.com>
2023-07-19 10:24:48 -0300

    Trying to improve eslint, add todo file

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import { app } from '@/app';
+import { app } from "@/app";
 
 app
   .listen({
     host: '0.0.0.0',
     port: 3333,
   })
-  .then(() => console.log('🎉 Server is running!'));
+  .then(() => console.log('🎉 Server is running!'))
